b8ec0c8250a30fe1fdd21cf53131962144598b90
Muhammad Awais <awaisrafique4929@gmail.com>
2026-04-08 23:17:44 +0500

    feat: initial commit

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { useState } from "react";
+import { createClient } from "../utils/supabase/client";
+
+export default function Page() {
+
+  const supabase = createClient();
+
+  const [session, setSession] = useState(supabase.auth.getSession());
+  const [mode, setMode] = useState("Login")
+
+  return (
+    <main>
+    </main>
+  );
+}
